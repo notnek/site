039b70ae358f3ab3203597bceac40d352f5c689b
Kenton Glass <notnek@users.noreply.github.com>
2020-07-18 09:48:26 -0400

    working on fixing the rss feed with nuxtjs/content

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,23 +3,12 @@ const siteDescription =
   'Kenton Glass is a full-stack developer in Louisville, KY';
 
 export default {
-  /*
-   ** Nuxt rendering mode
-   ** See https://nuxtjs.org/api/configuration-mode
-   */
   mode: 'universal',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
   target: 'static',
+  components: true,
   loading: {
     color: '#2B6CB0',
   },
-  /*
-   ** Headers of the page
-   ** See https://nuxtjs.org/api/configuration-head
-   */
   head: {
     title: siteTitle,
     meta: [
@@ -62,45 +51,13 @@ export default {
       },
     ],
   },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   ** https://nuxtjs.org/guide/plugins
-   */
   plugins: ['@/plugins/filters.js'],
-  /*
-   ** Auto import components
-   ** See https://nuxtjs.org/api/configuration-components
-   */
-  components: true,
-  /*
-   ** Nuxt.js dev-modules
-   */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
-    // Doc: https://github.com/nuxt-community/feed-module,
-    '@nuxtjs/feed',
-  ],
-  /*
-   ** Content module configuration
-   ** See https://content.nuxtjs.org/configuration
-   */
+  modules: ['@nuxtjs/pwa', '@nuxt/content', '@nuxtjs/feed'],
   content: {
     markdown: {
       prism: {
@@ -108,14 +65,9 @@ export default {
       },
     },
   },
-  /*
-   ** Feed module configuration
-   ** See https://github.com/nuxt-community/feed-module#configuration
-   */
   feed() {
     const { $content } = require('@nuxt/content');
     const baseUrlArticles = 'https://kenton.glass/articles';
-    const baseLinkFeedArticles = '/feed';
     const feedFormats = {
       rss: { type: 'rss2', file: 'rss.xml' },
       json: { type: 'json1', file: 'feed.json' },
@@ -132,21 +84,30 @@ export default {
 
       articles.forEach((article) => {
         const url = `${baseUrlArticles}/${article.slug}`;
+        let summary = article.body;
+
+        if (summary.length > 60) {
+          summary = summary.substring(0, 60);
+          summary = `${summary.substring(0, summary.lastIndexOf(' '))}…`;
+        }
+
+        console.log(summary);
+        console.log(article.body);
 
         feed.addItem({
           title: article.title,
           id: url,
           link: url,
-          date: article.published,
-          description: article.summary,
-          content: article.summary,
-          author: article.authors,
+          date: new Date(article.publishedAt),
+          description: summary,
+          content: article.body,
+          author: 'Kenton Glass',
         });
       });
     };
 
     return Object.values(feedFormats).map(({ file, type }) => ({
-      path: `${baseLinkFeedArticles}/${file}`,
+      path: `/feed/${file}`,
       type,
       create: createFeedArticles,
     }));
